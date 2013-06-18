19a3a3bbe8805743ed0938fab87787004ca8acba
Luís Oliveira <luis@r42.eu>
2013-06-18 11:03:49 +0100

    Move browser setup to init.el for use in org-only mode

--- init.el
+++ init.el
@@ -54,6 +54,31 @@
           (message url)
           (do-applescript (concat "open location \"" url "\"")))))
 
+;;;; Browsers
+
+(when siscog-p
+  (defvar browse-url-chm-program "d:/cygwin/opt/bin/KeyHH.exe")
+
+  (defvar browse-url-chm-program-args '("-emacs"))
+
+  (defun browse-url-chm (url &rest args)
+    (with-temp-buffer
+      (let ((process (apply 'start-process
+                            "CHMBrowser"
+                            nil
+                            browse-url-chm-program
+                            (append browse-url-chm-program-args (list url)))))
+        (process-kill-without-query process))))
+
+  (defun lbo:browse-url-internet-explorer (url &rest ignore)
+    (interactive "sURL: ")
+    (shell-command (concat "explorer " (shell-quote-argument url))))
+
+  (setq browse-url-browser-function
+        '(("\\.chm" . browse-url-chm)
+          ("pms.siscog.com" . lbo:browse-url-internet-explorer)
+          ("." . browse-url-default-windows-browser))))
+
 (defun toggle-show-trailing-whitespace ()
   (interactive)
   (setq show-trailing-whitespace
