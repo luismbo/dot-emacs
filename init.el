fa303cfd5cd17b8e26ab1a409f548aedff51f232
Luís Oliveira <luis@r42.eu>
2013-06-21 11:32:59 +0100

    Enable paredit-mode for elisp too.

--- init.el
+++ init.el
@@ -155,7 +155,9 @@
   "Minor mode for pseudo-structurally editing Lisp code."
   t)
 
-(add-hook 'lisp-mode-hook (lambda () (paredit-mode +1)))
+(add-hook 'lisp-mode-hook (lambda () (paredit-mode 1)))
+
+(add-hook 'emacs-lisp-mode (lambda () (paredit-mode 1)))
 
 ;; have RET automatically do indentation
 (eval-after-load 'paredit
