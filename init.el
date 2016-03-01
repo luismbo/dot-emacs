62bc64afb00e0854e048e8f786a2968a9e0f3618
Luís Oliveira <luis@kerno.org>
2016-03-01 14:30:20 +0000

    Custom settings

--- init.el
+++ init.el
@@ -624,3 +624,15 @@
   (eval-after-load 'langtool
                    `(progn
                       (setq langtool-language-tool-jar "d:/opt/LanguageTool/languagetool-commandline.jar"))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-pull-arguments (quote ("--rebase"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
