2d5156ac23cb44c1f1ce2f774f4d132ad2f985a7
Luís Oliveira <luis@kerno.org>
2016-03-09 17:20:00 +0000

    Enable narrow-to-region

--- init.el
+++ init.el
@@ -602,6 +602,7 @@
                       (setq langtool-language-tool-jar "d:/opt/LanguageTool/languagetool-commandline.jar"))))
 
 (put 'narrow-to-page 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -609,6 +610,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values (quote ((org-clock-into-drawer)))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
