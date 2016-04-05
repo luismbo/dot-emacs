3ae89d20cdcf7e4e93449f4419f2ad2515bb2281
Luís Oliveira <loliveira@common-lisp.net>
2016-04-06 00:02:12 +0100

    More safe-local-variable-values

--- init.el
+++ init.el
@@ -641,7 +641,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((org-clock-into-drawer)))))
+ '(safe-local-variable-values
+   (quote
+    ((intent-tabs-mode)
+     (c-style . K&R)
+     (org-clock-into-drawer)))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
