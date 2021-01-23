c66aeabed2687d1c3f509d832c0567b700ff2773
Luís Oliveira <luis@kerno.org>
2021-01-23 22:14:38 +0000

    Update MELPA URL

--- init.el
+++ init.el
@@ -46,7 +46,7 @@
 
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 (defvar lbo:*auto-refreshed-packages* nil
