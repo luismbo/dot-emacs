070c6c226415bb62b2280c028a7ea31aa80939d1
Luís Oliveira <luis@kerno.org>
2021-02-09 10:55:07 +0000

    Playing around with ligatures

--- init.el
+++ init.el
@@ -1001,6 +1001,41 @@ This command shares argument histories with \\[rgrep] and \\[grep]."
                                      current-alpha))))
   (set-frame-parameter (selected-frame) 'alpha value))
 
+;;;; Ligatures
+
+(use-package ligature
+  :load-path "~/src/elisp/ligature.el/"
+  :config
+  ;; Enable the "www" ligature in every possible major mode
+  (ligature-set-ligatures 't '("www"))
+  ;; Enable traditional ligature support in eww-mode, if the
+  ;; `variable-pitch' face supports it
+  (ligature-set-ligatures 'eww-mode '("ff" "fi" "ffi"))
+  ;; Enable all Cascadia Code ligatures in programming modes
+  (ligature-set-ligatures t ;; '(prog-mode slime-editing-mode)
+			  '("|||>" "<|||" "<==>" "<!--" "####" "~~>" "***" "||=" "||>"
+			    ":::" "::=" "=:=" "===" "==>" "=!=" "=>>" "=<<" "=/=" "!=="
+			    "!!." ">=>" ">>=" ">>>" ">>-" ">->" "->>" "-->" "---" "-<<"
+			    "<~~" "<~>" "<*>" "<||" "<|>" "<$>" "<==" "<=>" "<=<" "<->"
+			    "<--" "<-<" "<<=" "<<-" "<<<" "<+>" "</>" "###" "#_(" "..<"
+			    "..." "+++" "/==" "///" "_|_" "www" "&&" "^=" "~~" "~@" "~="
+			    "~>" "~-" "**" "*>" "*/" "||" "|}" "|]" "|=" "|>" "|-" "{|"
+			    "[|" "]#" "::" ":=" ":>" ":<" "$>" "==" "=>" "!=" "!!" ">:"
+			    ;; ">="
+			    ">>" ">-" "-~" "-|" "->" "--" "-<" "<~" "<*" "<|" "<:"
+			    "<$"
+			    ;; "<="
+			    "<>" "<-" "<<" "<+" "</" "#{" "#[" "#:" "#=" "#!"
+			    "##" "#(" "#?" "#_" "%%" ".=" ".-" ".." ".?" "+>" "++" "?:"
+			    "?=" "?." "??"
+			    ;; ";;"
+			    "/*" "/=" "/>" "//" "__" "~~"
+			    ;; "(*" "*)"
+			    "\\\\" "://"))
+  ;; Enables ligature checks globally in all buffers. You can also do it
+  ;; per mode with `ligature-mode'.
+  (global-ligature-mode -1))
+
 ;;;; custom
 
 (custom-set-variables
