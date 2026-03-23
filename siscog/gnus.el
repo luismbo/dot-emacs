16f0145acdf2a0568b27a7d259af351fac13438c
Luís Oliveira <luis@kerno.org>
2026-03-23 10:30:39 +0000

    Random GNUS stuff

--- siscog/gnus.el
+++ siscog/gnus.el
@@ -7,20 +7,21 @@
 (require 'nnir)
 
 ;; safer place to store passwords.
-(add-to-list 'auth-sources "w:/.authinfo")
+;; (add-to-list 'auth-sources "w:/.authinfo")
 
 ;; main SISCOG account.
 (setq gnus-select-method
       '(nnimap "SISCOG"
-        (nnimap-address "imaps.lisboa.siscog")
-        (nnimap-list-pattern "INBOX" "imap-mail/*")
+        (nnimap-address "outlook.office365.com")
+        ;; (nnimap-list-pattern "INBOX")
         (nnimap-expunge-on-close never) ; not sure what this does
         ;; this means we need to load everything using (kbd "A C")
         ;; before being able to save the attachment.
         (nnimap-fetch-partial-articles "text/")
         (nnir-search-engine imap)
         (nnimap-server-port 993)
-        (nnimap-stream ssl)))
+        (nnimap-stream ssl)
+        (nnimap-authenticator xoauth2)))
 
 ;; Always show some groups, whether they have new messages or not.
 ;;
@@ -35,11 +36,11 @@
 (setq gnus-message-archive-group "imap-mail/Sent")
 (setq gnus-gcc-mark-as-read t) ; mark them as read
 
-(add-to-list 'gnus-secondary-select-methods '(nntp "news.gmane.org"))
+(add-to-list 'gnus-secondary-select-methods '(nntp "news.gmane.io"))
 
-(add-to-list 'gnus-secondary-select-methods
-             ;; the nntp backend doesn't seem to look at auth-sources
-             '(nntp "news.sunsite.dk" (nntp-authinfo-file "w:/.authinfo")))
+;; (add-to-list 'gnus-secondary-select-methods
+;;              ;; the nntp backend doesn't seem to look at auth-sources
+;;              '(nntp "news.sunsite.dk" (nntp-authinfo-file "w:/.authinfo")))
 
 ;; (add-to-list 'gnus-secondary-select-methods
 ;;              '(nnimap "Gmail"
@@ -50,9 +51,9 @@
 ;; Make Gnus NOT ignore [Gmail] mailboxes
 ;; (setq gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
 
-(setq gnus-posting-styles
-      '(("gmane.*" ("From" "Luís Oliveira <luismbo@gmail.com>"))
-        ("comp.*" ("From" "Luís Oliveira <luis.oliveira@deadspam.com>"))))
+;; (setq gnus-posting-styles
+;;       '(("gmane.*" ("From" "Luís Oliveira <luismbo@gmail.com>"))
+;;         ("comp.*" ("From" "Luís Oliveira <luis.oliveira@deadspam.com>"))))
 
 ;; (setq gnus-invalid-group-regexp "[:`'\"]\\|^$")
 ;; (setq gnus-group-line-format "%M%S%p%P%5y:%B%(%G%)%l %O\n")
@@ -62,7 +63,6 @@
 (add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
 (setq gnus-topic-display-empty-topics nil)
 
-;; hide fugly pics.
 (setq gnus-treat-display-smileys nil)
 (setq gnus-treat-display-face nil)
 
@@ -75,10 +75,10 @@
 ;; for when I can't get Emacs to load libxml2.dll
 ;; also, it displays less colours, which is actually nice.
 ;; It is one of defined renderer types, or a rendering function.
-(setq mm-text-html-renderer 'shr)
+;(setq mm-text-html-renderer 'shr)
 ;; (setq mm-text-html-renderer 'gnus-w3m)
-(setq mm-inline-large-images t)
-(setq mm-inline-text-html-with-images t)
+;(setq mm-inline-large-images t)
+;(setq mm-inline-text-html-with-images t)
 ;; (setq mm-inline-text-html-with-w3m-keymap nil)
 
 ;;;; Keybindings
@@ -87,16 +87,17 @@
   'lbo:gnus-move-to-trash)
 
 (defun lbo:gnus-move-to-trash ()
-  (interactive) (gnus-summary-move-article nil "imap-mail/Trash" nil 'move))
+  (interactive) (gnus-summary-move-article nil "Deleted Items" nil 'move))
 
 (define-key gnus-summary-mode-map (kbd "a")
   'lbo:gnus-archive)
 
 (defun lbo:gnus-archive ()
-  (interactive) (gnus-summary-move-article nil "imap-mail/Archives" nil 'move))
+  (interactive) (gnus-summary-move-article nil "Archive" nil 'move))
 
 ;;;; Sending Mail
 
-(setq smtpmail-default-smtp-server "smtps.lisboa.siscog")
+(setq smtpmail-default-smtp-server "smtp.office365.com")
+(setq smtpmail-smtp-service 587)
 (setq smtpmail-local-domain nil)
 (setq message-send-mail-function 'smtpmail-send-it)
